d99e5a831b7c14ea2a22f5c4d3036869c17c8ea9
Dominik Rosser <dominik.rosser@protonmail.ch>
2024-05-17 12:09:32 +0200

    Install some zsh plugins...

--- .dotfiles-nix/home/domi/pkglist.nix
+++ .dotfiles-nix/home/domi/pkglist.nix
@@ -1,57 +1,110 @@
 { pkgs, inputs, ... }:
 let
-    #tree-sitter-nix = inputs.tree-sitter-nix-flake.packages.${pkgs.system}.tree-sitter-nix;
-in
-{
+  #tree-sitter-nix = inputs.tree-sitter-nix-flake.packages.${pkgs.system}.tree-sitter-nix;
+in {
   home.packages = with pkgs; ([
     direnv
 
     inputs.pyprland.packages."x86_64-linux".pyprland
-    
+
     # [BROWSERS]
-    brave	firefox qutebrowser w3m
+    brave
+    firefox
+    qutebrowser
+    w3m
 
     # [FILE MANAGERS]
-    gnome.nautilus nnn ranger
-    
+    gnome.nautilus
+    nnn
+    ranger
+
     # [GIT UTILS]
     meld
 
     # [MEDIA] video, audio, pictures, pdf, ...
-    vlc kdePackages.okular gimp zathura vimiv-qt
+    vlc
+    kdePackages.okular
+    gimp
+    zathura
+    vimiv-qt
 
     # [INTERNET] graphical gui tools, utils, vpn, ...
-    protonvpn-gui iwgtk networkmanagerapplet
+    protonvpn-gui
+    iwgtk
+    networkmanagerapplet
 
     # [BLUETOOTH] graphical gui tools
-    blueberry blueman bluedevil bluez bluez-tools bluez-alsa
+    blueberry
+    blueman
+    bluedevil
+    bluez
+    bluez-tools
+    bluez-alsa
 
     # [SOUND] graphical gui tools
-    pamixer pavucontrol pa-notify
+    pamixer
+    pavucontrol
+    pa-notify
 
     # [APP LAUNCHERS]
-    rofi-wayland fuzzel wofi
-    
+    rofi-wayland
+    fuzzel
+    wofi
+
     # [PRODUCTIVITY/WORK] apps
-    nb discord p3x-onenote libreoffice virtualbox xournalpp
+    nb
+    discord
+    p3x-onenote
+    libreoffice
+    virtualbox
+    xournalpp
 
     # [TERMINAL] prompts, ...
-    alacritty starship
+    alacritty
+    starship
+    fzf
+    zoxide
+    chroma
 
     # [GTK]
-    adw-gtk3 gtk3-x11 gradience
+    adw-gtk3
+    gtk3-x11
+    gradience
 
     # [QT]
-    kdePackages.qt6gtk2 qt5ct qt6ct pinentry-qt
+    kdePackages.qt6gtk2
+    qt5ct
+    qt6ct
+    pinentry-qt
 
     # [WAYLAND] utils
-    cliphist slurp swaylock swww waypaper swappy
-    wlogout wlsunset wl-gammactl wmctrl wf-recorder grimblast eww dunst
+    cliphist
+    slurp
+    swaylock
+    swww
+    waypaper
+    swappy
+    wlogout
+    wlsunset
+    wl-gammactl
+    wmctrl
+    wf-recorder
+    grimblast
+    eww
+    dunst
     wayland-pipewire-idle-inhibit # check if this has a service...
-    xclip wl-clipboard-rs xsel wayshot
+    xclip
+    wl-clipboard-rs
+    xsel
+    wayshot
 
     # [HYPRLAND] utils
-    hyprshot hyprpaper hypridle hyprcursor hyprlock hyprpicker
+    hyprshot
+    hyprpaper
+    hypridle
+    hyprcursor
+    hyprlock
+    hyprpicker
 
     # [TOOLS]
     chezmoi
@@ -59,36 +112,67 @@ in
     hplip
 
     # [TYPST]
-    typst vimPlugins.nvim-treesitter-parsers.typst typst-lsp typst-live
-    typst-preview typstfmt prettypst typst-fmt
+    typst
+    vimPlugins.nvim-treesitter-parsers.typst
+    typst-lsp
+    typst-live
+    typst-preview
+    typstfmt
+    prettypst
+    typst-fmt
 
     # [EMACS UTILS]
     nixfmt-classic
 
     # [PYTHON UTILS AND FORMATTERS]
-    black python312Packages.cython python312Packages.pyflakes isort python312Packages.pytest python311Packages.nose python312Packages.pyls-isort
-    python312Packages.python-lsp-server python312Packages.python-lsp-black python312Packages.pylsp-mypy
+    black
+    python312Packages.cython
+    python312Packages.pyflakes
+    isort
+    python312Packages.pytest
+    python311Packages.nose
+    python312Packages.pyls-isort
+    python312Packages.python-lsp-server
+    python312Packages.python-lsp-black
+    python312Packages.pylsp-mypy
 
     # [NVIM] utils / libraries mainly used for nvim
     # ...
 
     # [ICON THEMES]
-    gnome-icon-theme papirus-icon-theme numix-icon-theme iconpack-obsidian iconpack-jade
-    icon-library material-icons material-design-icons material-symbols papirus-icon-theme
+    gnome-icon-theme
+    papirus-icon-theme
+    numix-icon-theme
+    iconpack-obsidian
+    iconpack-jade
+    icon-library
+    material-icons
+    material-design-icons
+    material-symbols
+    papirus-icon-theme
 
     # [FONTS]
-    jetbrains-mono victor-mono udev-gothic iosevka rubik lexend nerdfonts
+    jetbrains-mono
+    victor-mono
+    udev-gothic
+    iosevka
+    rubik
+    lexend
+    nerdfonts
 
     # [STEAM]
     protonup-qt
 
-
     lxqt.lxqt-openssh-askpass # GUI to query passwords on behalf of SSH agents.
 
-    python311Packages.material-color-utilities python311Packages.matplotlib
-    python311Packages.pipx pipx pywal python311Packages.pywayland
-    python311Packages.setuptools-scm nodePackages.pyright
-
+    python311Packages.material-color-utilities
+    python311Packages.matplotlib
+    python311Packages.pipx
+    pipx
+    pywal
+    python311Packages.pywayland
+    python311Packages.setuptools-scm
+    nodePackages.pyright
 
     vimPlugins.coc-pyright # probably only need the one from nodePackages... maybe remove
     # REM30 smplayer  # A complete front-end for MPlayer
@@ -103,12 +187,12 @@ in
 
     # x11_ssh_askpass
 
-    ### KDE STUFF ### 
+    ### KDE STUFF ###
     # REM30 spectacle
     # REM30 plasma-browser-integration
 
     kdePackages.plasma-wayland-protocols
 
     # REM30 polkit-kde-agent
-  ]);# ++ [ tree-sitter-nix ];
+  ]); # ++ [ tree-sitter-nix ];
 }
--- .zshrc
+++ .zshrc
@@ -1,13 +1,76 @@
+# install the package manager for our zsh
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
+# init
 eval "$(starship init zsh)"
 eval "$(direnv hook zsh)"
 
-alias ls="ls --color=auto"
-alias ll="ls -l --color=auto"
+# Add zsh plugins
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+
+# load the zsh completions when zsh starts
+autoload -U compinit && compinit
+zinit cdreplay -q
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+
+# hotkey for completing zsh-autosuggestions
+bindkey '^f' autosuggest-accept
+
+# History
+HISTSIZE=10000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+# Init fuzzyfinder support
+eval "$(fzf --zsh)"
+# Init zoxide
+eval "$(zoxide init --cmd cd zsh)"
+
+# Add other plugins
+# From oh my zsh repository:
+zinit snippet OMZP::git
+zinit snippet OMZP::dotenv
+zinit snippet OMZP::gpg-agent
+zinit snippet OMZP::git-auto-fetch
+zinit snippet OMZP::sudo
+zinit snippet OMZP::command-not-found
+# fails: zinit snippet OMZP::ag
+# fails: zinit snippet OMZP::bun
+zinit snippet OMZP::cabal
+zinit snippet OMZP::direnv
+zinit snippet OMZP::gcloud
+zinit snippet OMZP::copyfile
+zinit snippet OMZP::catimg
+zinit snippet OMZP::colored-man-pages
+zinit snippet OMZP::common-aliases
+zinit snippet OMZP::fancy-ctrl-z
+zinit snippet OMZP::colorize
+export ZSH_COLORIZE_TOOL=chroma
+alias cat="ccat"
+
+# aliases
+alias ls="ls --color"
+alias ll="ls -l --color"
 alias grep="grep --color=auto"
 alias ..="cd .."
 alias dotfilesgit="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+alias dit="dotfilesgit"
